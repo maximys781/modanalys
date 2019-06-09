90c4e772e41622f7caeb1f5e8d4bd4b19705a045
maximys781 <43477150+maximys781@users.noreply.github.com>
2019-06-10 00:40:00 +0300

    Add files via upload

--- src/Controller/Report/UserDevices.php
+++ src/Controller/Report/UserDevices.php
@@ -37,12 +37,12 @@ class UserDevices extends ControllerBase {
     $header = $this->_getHeader();
 
     return array(
-      '#title' => SafeMarkup::checkPlain(t('User with IP:') . ' ' . $host),
+      '#title' => SafeMarkup::checkPlain(t('Устройства пользователей')),
       'modanalys_date_filter_form' => $form,
       'modanalys_table' => array(
         '#type'  => 'table',
         '#header' => $header,
-        '#rows'   => $this->_getData($header, $host),
+        '#rows'   => $this->_getData($header),
       ),
       'modanalys_pager' => array('#type' => 'pager')
     );
@@ -66,10 +66,10 @@ class UserDevices extends ControllerBase {
         'specifier' => 'modanalys_date_time',
         'class'     => array(RESPONSIVE_PRIORITY_LOW),
       ),
-      'devperson_url' => array(
-        'data'      => t('URL'),
-        'field'     => 'devperson_url',
-        'specifier' => 'devperson_url',
+      'devperson_ip' => array(
+        'data'      => t('IP'),
+        'field'     => 'devperson_ip',
+        'specifier' => 'devperson_ip',
         'class'     => array(RESPONSIVE_PRIORITY_LOW),
       ),
       'u.name' => array(
@@ -79,30 +79,22 @@ class UserDevices extends ControllerBase {
         'class'     => array(RESPONSIVE_PRIORITY_LOW),
       ),
       '' => array(
-        'data'      => t('Operations'),
+        'data'      => t('Devices'),
       ),
     );
   }
 
   
-  protected function _getData($header, $host) {
-    if (@inet_pton($host) === FALSE) //inet_pton занимается проверкой IPv6 адресов на соответствие
-    {
-      return;
-    }
-
-    $items_per_page = \Drupal::config('modanalys.config')->get('items_per_page');
-
+  protected function _getData($header) {
+    $items_per_page = \Drupal::config('visitors.config')->get('items_per_page');
     $query = db_select('devices', 'd')
       ->extend('Drupal\Core\Database\Query\PagerSelectExtender')
       ->extend('Drupal\Core\Database\Query\TableSortExtender');
-
-    $query->leftJoin('users_field_data', 'u', 'u.uid=d.devperson_uid');// делаем левое соединение, где один кортеж из одной таблицы связываем с другой
+    $query->leftJoin('users_field_data', 'u', 'u.uid=d.devperson_id');
     $query->fields(
       'd',
       array(
         'devperson_id',
-        'devperson_ip',
         'devperson_uid',
         'modanalys_date_time',
         'devperson_title',
@@ -111,52 +103,38 @@ class UserDevices extends ControllerBase {
       )
     );
     $query->fields('u', array('name', 'uid'));
-    $query->condition('d.devperson_ip', sprintf('%u', ip2long($host)), '=');
     modanalys_date_filter_sql_condition($query);
     $query->orderByHeader($header);
     $query->limit($items_per_page);
 
-    /*$count_query = db_select('devperson', 'd');
+    $count_query = db_select('devices', 'd');
     $count_query->addExpression('COUNT(*)');
-    $count_query->condition('visitors_ip', sprintf('%u', ip2long($host)));
-    visitors_date_filter_sql_condition($count_query);
-    $query->setCountQuery($count_query);*/
+    modanalys_date_filter_sql_condition($count_query);
+    $query->setCountQuery($count_query);
     $results = $query->execute();
 
-    /*$count = $count_query->execute()->fetchField();
-    if ($count == 0) {
-      return;
-    }*/
     $rows = array();
 
-    $page = isset($_GET['page']) ? $_GET['page'] : '';
+    $page = isset($_GET['page']) ? (int) $_GET['page'] : '';
     $i = 0 + $page * $items_per_page;
-
+    $timezone =  drupal_get_user_timezone();
     foreach ($results as $data) {
       $user = user_load($data->devperson_uid);
-      $username = array('#type' => 'username', '#account' => $user);
-
-      $modanalys_host_url = Url::fromRoute('modanalys.hit_details',array("devperson_id"=>$data->devperson_id));
-      $modanalys_host_link = \Drupal::l($this->t('details'),\Drupal\Core\Url::fromRoute('modanalys.hit_details',array("devperson_id"=>$data->devperson_id)));
-      $modanalys_host_link = $modanalys_host_link->toRenderable();
-
-
-      $user_profile_url = Url::fromRoute('entity.user.canonical',array("user"=>$user->id()));
-      $user_profile_link = Link::fromTextAndUrl($user->getAccountName(),$user_profile_url);
-      $user_profile_link = $user_profile_link->toRenderable();
-
+      $username = array(
+        '#type' => 'username',
+        '#account' => $user
+      );
       $rows[] = array(
         ++$i,
         $data->devperson_id,
         $this->date->format($data->modanalys_date_time, 'short'),
-        SafeMarkup::checkPlain($data->devperson_title) . '<br/>',
-        render($user_profile_link),
-        render($modanalys_host_link)
-        
+        $data->devperson_path,
+        $user->getAccountName(),
+        //\Drupal::l(t('details'), \Drupal\Core\Url::fromUri('visitors/hits/' . $data->visitors_id))
+        \Drupal::l($this->t('details'),\Drupal\Core\Url::fromRoute('modanalys.hit_details',array("dev_id"=>$data->devperson_id)))
       );
     }
 
     return $rows;
   }
 }
-
